14dba4ebdbb346d0b6c1f68e9136b47352ba05e5
ChunguangZha <xiaocha0209@hotmail.com>
2016-10-18 17:32:36 +0800

    1018 from c

--- SuperMinersWPF/SuperMinersWPF/GlobalData.cs
+++ SuperMinersWPF/SuperMinersWPF/GlobalData.cs
@@ -56,9 +56,9 @@ namespace SuperMinersWPF
             }
             else
             {
-                decimal tempOutputStone = CurrentUser.TempOutputStones;
+                //decimal tempOutputStone = CurrentUser.TempOutputStones;
                 CurrentUser.ParentObject = user;
-                CurrentUser.TempOutputStones = tempOutputStone;
+                //CurrentUser.TempOutputStones = tempOutputStone;
             }
         }
 
