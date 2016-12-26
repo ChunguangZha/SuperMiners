374240b043405870e897037e38b92af4a12bfd1d
xiaocha0209@hotmail.com <buzhidaoGIT123>
2016-12-26 22:05:16 +0800

    1226 f h

--- SuperMinersServerApplication/SuperMinersServerApplication/Controller/Game/RaidersofLastArkController.cs
+++ SuperMinersServerApplication/SuperMinersServerApplication/Controller/Game/RaidersofLastArkController.cs
@@ -1,9 +1,13 @@
 ﻿using MetaData;
 using MetaData.Game.RaideroftheLostArk;
+using MetaData.User;
+using SuperMinersServerApplication.Utility;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SuperMinersServerApplication.Controller.Game
@@ -18,19 +22,33 @@ namespace SuperMinersServerApplication.Controller.Game
             private set { _parentObject = value; }
         }
 
-        private List<PlayerBetInfo> listPlayerBetInfos = new List<PlayerBetInfo>();
+        private ConcurrentQueue<PlayerBetInfo> queuePlayerBetInfos = new ConcurrentQueue<PlayerBetInfo>();
 
         public RaiderRoundLogicRunner(RaiderRoundMetaDataInfo parent)
         {
             this.ParentObject = parent;
         }
 
-
+        public bool Join(PlayerInfo player, int stonesCount)
+        {
+            queuePlayerBetInfos.Enqueue(new PlayerBetInfo()
+            {
+                UserID = player.SimpleInfo.UserID,
+                RaiderRoundID = ParentObject.ID,
+                BetStones = stonesCount,
+                Time = MyDateTime.FromDateTime(DateTime.Now)
+            });
+            return true;
+        }
     }
 
     public class RaidersofLastArkController
     {
         private RaiderRoundLogicRunner _currentRoundRunner;
+
+        private ConcurrentQueue<RaiderRoundLogicRunner> _queueRunner = new ConcurrentQueue<RaiderRoundLogicRunner>();
+
+        private Thread _thrRound = null;
 
         private int _lastRoundID = 0;
 
@@ -44,7 +62,32 @@ namespace SuperMinersServerApplication.Controller.Game
                 StartTime = MetaData.MyDateTime.FromDateTime(DateTime.Now),
             });
 
+            _thrRound = new Thread(new ThreadStart(FinishRound));
+            _thrRound.IsBackground = true;
+            _thrRound.Name = "RaidersofLastArkController";
+            _thrRound.Start();
 
+        }
+
+        private void FinishRound()
+        {
+            while (true)
+            {
+                try
+                {
+                    if (_queueRunner.Count == 0)
+                    {
+                        Thread.Sleep(100);
+                        continue;
+                    }
+
+
+                }
+                catch (Exception exc)
+                {
+                    LogHelper.Instance.AddErrorLog("RaidersofLastArkController Thread Round Exception", exc);
+                }
+            }
         }
 
         public OperResultObject Join(MetaData.User.PlayerInfo player, int stones)
